5bbebc6a7a9228d5af1873a9052e5c4f4de8a3b0
kasikcimurat <kasikcimurat@icloud.com>
2026-04-29 22:19:27 +0300

    Delete log.php

--- log.php
+++
@@ -1,8 +0,0 @@
-<?php
-$isim = $_POST["isim"] ?? "Bilinmeyen";
-$tarih = date("d.m.Y H:i:s");
-
-$satir = "Ad: $isim | Tarih: $tarih\n";
-
-file_put_contents("girisLog.txt", $satir, FILE_APPEND);
-?>
